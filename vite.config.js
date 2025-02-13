08108260075ab84168a87dfe0444fb440f5bbefd
wolverine0606 <dimaspotapov0606@gmail.com>
2025-02-13 13:52:17 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb', '**/*.gltf'],
-})
+  assetsInclude: ["**/*.glb", "**/*.gltf"],
+  base: "/3d-portfolio/",
+});
