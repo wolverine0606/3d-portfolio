c9dabf335819ac4f4bae23b07411946949321fbd
wolverine0606 <dimaspotapov0606@gmail.com>
2025-03-24 08:04:29 +0100

    fix name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,15 @@ import Position from "./Position";
 const Hero = ({ scrollContainer }) => {
   return (
     <section className="parallax">
-      <div className="parallax__content absolute top-[10%] sm:top-[16%] lg:top-[24%] w-full mx-auto lg:pl-[38vh] lg:pr-[30vh] xl:pl-96 xl:pr-72 2xl:px-40 3xl:px-60 flex flex-col lg:flex-row items-start z-10">
+      <div className="parallax__content absolute top-[10%] sm:top-[16%] lg:top-[24%] w-full mx-auto px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 2xl:px-40 flex flex-col lg:flex-row items-start z-10">
         <div className="flex-1 lg:mb-0">
-          <h1 className="font-medium text-white text-[40px] xs:text-[50px] sm:text-[68px] md:text-[80px] lg:text-[100px] 2xl:text-[180px] leading-[110px] 2xl:leading-[160px]">
+          <h1 className="font-medium text-white text-[40px] xs:text-[50px] sm:text-[68px] md:text-[80px] lg:text-[100px] 2xl:text-[180px] leading-[110px] 2xl:leading-[160px] text-center lg:text-left">
             POTAPOV DMYTRO
           </h1>
           <Position />
         </div>
-        <div className="flex-1 flex justify-start lg:justify-end mt-4 sm:mt-14 ml-8 xs:ml-[-4vh] sm:ml-[-17vh] md:ml-[-26vh] lg:mt-10 2xl:mt-0">
-          <div className="font-bold text-[20px] sm:text-[30px] md:text-[36px] 2xl:text-[46px] sm:leading-[40px] md:leading-[50px] 2xl:leading-[60px] streaky-glow max-w-sm 2xl:max-w-lg text-white text-left">
+        <div className="flex-1 flex justify-center lg:justify-end mt-4 sm:mt-14 lg:mt-10">
+          <div className="font-bold text-[20px] sm:text-[30px] md:text-[36px] 2xl:text-[46px] sm:leading-[40px] md:leading-[50px] 2xl:leading-[60px] streaky-glow max-w-sm 2xl:max-w-lg text-white text-center lg:text-left">
             Creating reliable and <br /> user-friendly mobile experiences with
             React Native.
           </div>
